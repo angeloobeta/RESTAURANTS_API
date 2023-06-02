d973864ee87dfb1894e7d080561b5c0c0e568600
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-06-02 19:35:48 +0100

    Create: Create user schema

---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+@Schema()
+export class User extends Document {
+  @Prop()
+  name: string;
+
+  @Prop({ unique: [true, 'Duplicate email entered'] })
+  email: string;
+
+  @Prop({ select: false })
+  password: string;
+
+  @Prop({
+    enum: UserRole,
+    default: UserRole.USER,
+  })
+  role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
