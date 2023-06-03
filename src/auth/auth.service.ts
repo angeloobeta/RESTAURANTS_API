4a60cfbb9a2d7a3fe08f18ff23c28832e456051c
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-06-03 07:12:09 +0100

    Added: Implemented hashing of user-password upon sign-up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import * as mongoose from 'mongoose';
 import { SignUpDto } from './dto/signup.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -14,10 +15,11 @@ export class AuthService {
   // Register User
   async signUp(signUPDto: SignUpDto): Promise<User> {
     const { name, email, password } = signUPDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user = await this.userModel.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
     return user;
   }
