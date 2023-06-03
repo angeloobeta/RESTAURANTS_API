c22dbb1195727fc5695841eb0045329d24e752bb
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-06-03 07:22:28 +0100

    Added: Implemented Handling of duplicate email upon sign-up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from "@nestjs/common";
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import * as mongoose from 'mongoose';
@@ -16,11 +16,20 @@ export class AuthService {
   async signUp(signUPDto: SignUpDto): Promise<User> {
     const { name, email, password } = signUPDto;
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await this.userModel.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
-    return user;
+
+    try {
+      const user = await this.userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
+      return user;
+    } catch (e) {
+      // Handling duplicate error
+      if (e.code == 11000) {
+        console.log(e.message);
+        throw new ConflictException('This email has already been used');
+      }
+    }
   }
 }
