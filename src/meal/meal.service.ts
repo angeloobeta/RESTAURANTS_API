f9b1f8a16db77428324cccc33632b8ad2cc5d592
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-06-05 21:11:31 +0100

    Added: Bug fixing and refactoring

--- src/meal/meal.service.ts
+++ src/meal/meal.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from '@nestjs/mongoose';
+import { Meal } from './schema/meal.schema';
+import * as mongoose from 'mongoose';
+import { User } from '../auth/schemas/user.schema';
+import { Restaurant } from '../restaurants/schemas/restaurants.schema';
 
 @Injectable()
-export class MealService {}
+export class MealService {
+  constructor(
+    @InjectModel(Meal.name)
+    private mealModel: mongoose.Model<Meal>,
+    @InjectModel(Restaurant.name)
+    private restaurantModel: mongoose.Model<Restaurant>,
+  ) {}
+
+
+  // create a new meal
+  async create(meal: Meal, user: User): Promise<Meal>{
+    const data = Object.assign(meal, { user: user._id });
+    const mealCreated = await this.mealModel.create(data);
+
+    if(!mealCreated){
+      throw new NotFoundException('Restaurant found by 2d');
+    }
+
+    // saving meal Id in the restaurant erra
+  }
+}
