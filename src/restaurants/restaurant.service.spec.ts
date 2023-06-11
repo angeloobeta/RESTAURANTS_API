1b03956ec86ea24e06f2539016cc51d5a10f2b2e
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-06-11 18:20:15 +0100

    Test: First unit-test on the Restaurant service using the "toBeDefined" method

---
+++ src/restaurants/restaurant.service.spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule} from '@nestjs/testing';
+import { RestaurantsService } from './restaurants.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Restaurant } from './schemas/restaurants.schema';
+import { Model } from 'mongoose';
+
+const mockRestaurantService = {};
+describe('RestaurantService', () => {
+  let service: RestaurantsService;
+  let model: Model<Restaurant>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        RestaurantsService,
+        {
+          provide: getModelToken(Restaurant.name),
+          useValue: mockRestaurantService,
+        },
+      ],
+    }).compile();
+
+    service = module.get<RestaurantsService>(RestaurantsService);
+    model = module.get<Model<Restaurant>>(getModelToken(Restaurant.name));
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
